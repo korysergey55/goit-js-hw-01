a42d1fbf49bb9e0ca3fdcf791c586e6567d59c0c
Serhiy Korytskyy <korysergey55@gmail.com>
2020-12-15 09:53:51 +0200

    first commit

---
+++ task-02.js
@@ -0,0 +1,13 @@
+
+let total;
+let ordered;
+
+total = 100;
+ordered = 50;
+
+if (total > ordered) {
+    alert("Заказ оформлен, с вами свяжется менеджер")
+}
+else {
+    alert ("На складе недостаточно твоаров!")
+};
